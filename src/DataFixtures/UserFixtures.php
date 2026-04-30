3bb0c4edc8d0070283cc20b56d7896d07a029b26
Auxioma <auxioma@yandex.fr>
2026-04-30 21:18:47 +0200

    fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -15,17 +15,71 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Intl\Countries;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $faker = Faker\Factory::create('fr_FR');
+
         $visiteur = new User()
             ->setEmail('visiteur@visiteur.visiteur')
             ->setRoles(['ROLE_USER'])
             ->setIsVerified(true)
+            ->setNom($faker->lastName())
+            ->setPrenom($faker->firstName())
+            ->setPassword($this->passwordHasher->hashPassword($visiteur, 'Test1234!'))
         ;
         $manager->persist($visiteur);
+
+        $agence = new User()
+            ->setEmail('agence@agence.agence')
+            ->setRoles(['ROLE_USER'])
+            ->setIsVerified(true)
+            ->setNom($faker->lastName())
+            ->setPrenom($faker->firstName())
+            ->setPassword($this->passwordHasher->hashPassword($agence, 'Test1234!'))
+        ;
+        $manager->persist($agence);
+
+        $admin = new User()
+            ->setEmail('admin@admin.admin')
+            ->setRoles(['ROLE_USER'])
+            ->setIsVerified(true)
+            ->setNom($faker->lastName())
+            ->setPrenom($faker->firstName())
+            ->setPassword($this->passwordHasher->hashPassword($admin, 'Test1234!'))
+        ;
+        $manager->persist($admin);
+
+        $countries = Countries::getNames('fr');
+        foreach ($countries as $countryCode => $countryName) {
+            for ($i = 1; $i <= 2; ++$i) {
+                $agence = new User();
+
+                $agence
+                    ->setEmail(sprintf('agence%d.%s@boolts.test', $i, mb_strtolower($countryCode)))
+                    ->setRoles(['ROLE_AGENCE']) // ou ['ROLE_USER'] si tu n'as pas encore ROLE_AGENCE
+                    ->setIsVerified(true)
+                    ->setNom($faker->lastName())
+                    ->setPrenom($faker->firstName())
+                    ->setPays($countryName)
+                ;
+
+                $agence->setPassword($this->passwordHasher->hashPassword($agence, 'Test1234!'));
+
+                $manager->persist($agence);
+            }
+        }
+
+        
 
         $manager->flush();
     }
