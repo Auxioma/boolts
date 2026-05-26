268784ead92c80c26c40c9bfdbf038bc86fa456e
Auxioma <auxioma@yandex.fr>
2026-05-26 10:04:56 +0200

    btn option

---
+++ migrations/Version20260525140205.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260525140205 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE property ADD montant_loyer_hors_charge VARCHAR(255) DEFAULT NULL, ADD montant_depot_de_garantie VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE property DROP montant_loyer_hors_charge, DROP montant_depot_de_garantie');
+    }
+}
